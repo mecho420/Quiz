674347f351fa3588bbc42c3b6568d40636184704
mecho420 <123552939+mecho420@users.noreply.github.com>
2023-06-26 17:51:52 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,39 +7,75 @@ public class Main {
     public static void mathCategory () {
         Scanner scan = new Scanner(System.in);
         ArrayList<String> mathList = new ArrayList<String>();
+        int answer;
+        int points = 0;
         try {
-            File file = new File("test1.txt");
-            Scanner fileReader = new Scanner(file, "utf-8");
-            while (fileReader.hasNextLine()) {
-                System.out.println();
-                System.out.printf(fileReader.nextLine());
-            }
-            mathList.add(String.valueOf(file));
-            System.out.println();
+            mathList.add("Въпрос 1: Колко е 3 + 4 - 2 - 2 + 1 = ?, 1. три, 2. четири, 3. две, 4. едно");
+            int mathCategoryIndex = 0;
+            String mathQuestionsAndAnswers = mathList.get(mathCategoryIndex);
+            String[] mathData = mathQuestionsAndAnswers.split(", ");
+            String question1 = mathData[0];
+            String wrongAnswer1For1Question = mathData[1];
+            String correctAnswerFor1Question = mathData[2];
+            String wrongAnswer2For1Question = mathData[3];
+            String wrongAnswer3For1Question = mathData[4];
+            System.out.println(question1);
+            System.out.println(wrongAnswer1For1Question + "\n" +
+                    correctAnswerFor1Question + "\n" +
+                    wrongAnswer2For1Question + "\n" +
+                    wrongAnswer3For1Question);
+
             System.out.print("Отговор: ");
-            int answer = scan.nextInt();
+            answer = scan.nextInt();
+            System.out.println();
             if (answer == 2) {
-                System.out.println("Правилно!");
+                points++;
+                System.out.println("Правилно, твоят резултат за сега е " + points);
             } else if (answer != 2) {
-                System.out.println("Грешно!");
+                System.out.println("Грешно");
+                System.exit(0);
+            }
+
+            mathList.add("Въпрос 1: Колко е 3 + 4 - 2 - 2 + 1 = ?, 1. три, 2. четири, 3. две, 4. едно");
+            int mathCategoryIndex2 = 0;
+            String mathQuestionsAndAnswers2 = mathList.get(mathCategoryIndex2);
+            String[] mathData2 = mathQuestionsAndAnswers2.split(", ");
+            String question2 = mathData2[0];
+            String wrongAnswer1For2Question = mathData2[1];
+            String wrongAnswer2For2Question = mathData2[2];
+            String correctAnswerFor2Question = mathData2[3];
+            String wrongAnswer3For2Question = mathData2[4];
+            System.out.println(question2);
+            System.out.println(wrongAnswer1For2Question + "\n" +
+                    wrongAnswer2For2Question + "\n" +
+                    correctAnswerFor2Question + "\n" +
+                    wrongAnswer3For2Question);
+
+            System.out.print("Отговор: ");
+            answer = scan.nextInt();
+            System.out.println();
+            if (answer == 3) {
+                points++;
+                System.out.println("Правилно, твоят резултат за сега е " + points);
+            } else if (answer != 3) {
+                System.out.println("Грешно");
+                System.exit(0);
             }
         } catch (Exception ex) {
             System.out.println(ex);
         }
     }
 
-    public static void answer () {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Отговор: ");
-        int answer = scan.nextInt();
-        if (answer == 2) {
-            System.out.println("Правилно!");
-        } else if (answer != 2) {
-            System.out.println("Грешно!");
-        }
-    }
-
     public static void main(String[] args) {
-        mathCategory();
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Избери си категория от \n" +
+                "Математика \n" +
+                "История \n" +
+                "Биология: ");
+        String chooseCategory = scan.next();
+        if (chooseCategory.equals("Математика")) {
+            System.out.println();
+            mathCategory();
+        }
     }
 }
