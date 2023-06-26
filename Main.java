1743b2869606ed48498a1eab75be68bbb26017f3
mecho420 <123552939+mecho420@users.noreply.github.com>
2023-06-26 16:53:35 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,27 @@
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void mathCategory () {
+        Scanner scan = new Scanner(System.in);
+        ArrayList<String> mathList = new ArrayList<String>();
         try {
             File file = new File("test1.txt");
             Scanner fileReader = new Scanner(file, "utf-8");
             while (fileReader.hasNextLine()) {
                 System.out.println();
                 System.out.printf(fileReader.nextLine());
+            }
+            mathList.add(String.valueOf(file));
+            System.out.println();
+            System.out.print("Отговор: ");
+            int answer = scan.nextInt();
+            if (answer == 2) {
+                System.out.println("Правилно!");
+            } else if (answer != 2) {
+                System.out.println("Грешно!");
             }
         } catch (Exception ex) {
             System.out.println(ex);
@@ -28,10 +40,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        try {
-            String readMath = mathCategory();
-        } catch (Exception ex) {
-            System.out.println(ex);
-        }
+        mathCategory();
     }
 }
